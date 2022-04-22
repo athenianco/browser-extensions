8cc1fc4c88b6528af1da0136835201918cc206c5
Vadim Markovtsev <vadim@athenian.co>
2022-04-22 23:34:31 +0200

    Improve showing the time in hints

--- chrome/insert_sidebar_item.js
+++ chrome/insert_sidebar_item.js
@@ -1,3 +1,17 @@
+function hh_mm_ss(seconds) {
+  let date = new Date(null);
+  date.setSeconds(seconds);
+  let subday = date.toISOString().slice(11, 19);
+  let subday_sec = (subday.slice(0, 2) | 0) * 3600 + (subday.slice(3, 5) | 0) * 60 + (subday.slice(6) | 0);
+  let days = (seconds - subday_sec) / (24 * 3600);
+  if (days > 0) {
+    days = `${days}d `;
+  } else {
+    days = "";
+  }
+  return `${days}${subday}`;
+}
+
 function insertPRPipelineTimings(model) {
   if (model.data.length == 0) {
     setTimeout(queryPRTimings, 60000);
@@ -21,7 +35,7 @@ function insertPRPipelineTimings(model) {
   }
   max = Object.values(deploy_timings).reduce((max, v) => (max > v? max : v), 0);
   let deploy_bars = Object.entries(deploy_timings).map(([env, time]) => `         <div><span class="text-uppercase">Deploy to ${env}</span></div>
-         <div title="${time} seconds" class="athenian-progress athenian-deploy" style="width: ${(time / max) * 100}%;"></div>`);
+         <div title="${hh_mm_ss(time)}" class="athenian-progress athenian-deploy" style="width: ${(time / max) * 100}%;"></div>`);
   sidebar_item = `<div class="discussion-sidebar-item js-discussion-sidebar-item">
       <details class="details-reset details-overlay select-menu hx_rsm" id="athenian">    
         <summary class="discussion-sidebar-heading discussion-sidebar-toggle" aria-label="Delivery Pipeline - Athenian" role="button">
@@ -33,13 +47,13 @@ function insertPRPipelineTimings(model) {
       </details>
       <div class="css-truncate">
          <div><span class="text-uppercase">Work in progress</span></div>
-         <div title="${timings["wip"][0]} seconds" class="athenian-progress athenian-wip"></div>
+         <div title="${hh_mm_ss(timings["wip"][0])}" class="athenian-progress athenian-wip"></div>
          <div><span class="text-uppercase">Review</span></div>
-         <div title="${timings["review"][0]} seconds" class="athenian-progress athenian-review"></div>
+         <div title="${hh_mm_ss(timings["review"][0])}" class="athenian-progress athenian-review"></div>
          <div><span class="text-uppercase">Merge</span></div>
-         <div title="${timings["merge"][0]} seconds" class="athenian-progress athenian-merge"></div>
+         <div title="${hh_mm_ss(timings["merge"][0])}" class="athenian-progress athenian-merge"></div>
          <div><span class="text-uppercase">Release</span></div>
-         <div title="${timings["release"][0]} seconds" class="athenian-progress athenian-release"></div>
+         <div title="${hh_mm_ss(timings["release"][0])}" class="athenian-progress athenian-release"></div>
          ${deploy_bars.join("\n")}
          <style>
            .athenian-progress {
